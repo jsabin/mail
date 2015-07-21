07d9e982bb668e7201c4ebcdb153584996505bcc
Jeff Sabin <jsabin@proofpoint.com>
2015-07-21 14:01:17 -0600

    Changed pom to create uber executable jar. Made username and password optional.

--- src/main/java/org/jsabin/Email.java
+++ src/main/java/org/jsabin/Email.java
@@ -11,16 +11,27 @@ public class Email {
     public Email(String smtpServer, int smtpPort) {
         props.put("mail.smtp.host", smtpServer);
         props.put("mail.smtp.port", smtpPort);
-        props.put("mail.smtp.auth", true);
         props.put("mail.smtp.starttls.enable", true);
     }
 
     public void send(final String username, final String password, String from, String emailAddresses, String subject, String body) throws MessagingException {
-        Session session = Session.getInstance(props, new javax.mail.Authenticator() {
-            protected PasswordAuthentication getPasswordAuthentication() {
-                return new PasswordAuthentication(username, password);
-            }
-        });
+
+        Session session;
+        if (password != null && password.length() > 0) {
+            Authenticator authenticator = new Authenticator() {
+                protected PasswordAuthentication getPasswordAuthentication() {
+                    return new PasswordAuthentication(username, password);
+                }
+            };
+            props.put("mail.smtp.auth", true);
+            session = Session.getInstance(props, authenticator);
+        }
+        else
+        {
+            props.put("mail.smtp.auth", false);
+            session = Session.getInstance(props);
+        }
+
         MimeMessage message = new MimeMessage(session);
 
         message.setSubject(subject);
